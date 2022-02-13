0124b956e34095df25ad690f3a9928c601506b6a
itsanti <kurov.aleksandr+github@gmail.com>
2022-02-13 18:18:07 +0300

    Добавлены статика и админка для модели Post

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    # Тип: TextField
+    text = models.TextField()
+
+    # Тип поля: DateTimeField, для хранения даты и времени;
+    # параметр auto_now_add определяет, что в поле будет автоматически
+    # подставлено время и дата создания новой записи
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    # Тип: ForeignKey, ссылка на модель User
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': 'Это главная страница проекта Yatube',
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse('посты, отфильтрованные по группам')
+    context = {
+        'title': 'Группы проекта Yatube',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, 'posts/group_list.html', context)

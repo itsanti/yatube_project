b4a08a5dfea670110c199c7727c4eca875c22693
itsanti <kurov.aleksandr+github@gmail.com>
2022-02-14 15:13:13 +0300

    Доработка проекта к проверке

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,34 +5,28 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50)
-    description = models.TextField()
+    title = models.CharField('Имя', max_length=200)
+    slug = models.SlugField('Адрес', max_length=100)
+    description = models.TextField('Описание')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    # Тип: TextField
-    text = models.TextField()
-
-    # Тип поля: DateTimeField, для хранения даты и времени;
-    # параметр auto_now_add определяет, что в поле будет автоматически
-    # подставлено время и дата создания новой записи
-    pub_date = models.DateTimeField(auto_now_add=True)
-
-    # Тип: ForeignKey, ссылка на модель User
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор',
     )
-
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Сообщество',
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Group, Post
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Это главная страница проекта Yatube',
+        'title': 'Последние обновления на сайте',
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
